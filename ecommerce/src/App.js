b35262fe05ce1d2c6ff689bb7d00580123498203
Susan-zz <susan_zz@163.com>
2019-07-01 14:29:33 +0800

    引入antd

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import Button from 'antd/es/button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button type="primary">Button</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
