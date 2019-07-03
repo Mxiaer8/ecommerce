a2a294f69020e64b62360ea111bb328ba8cd6356
Mxiaer8 <905594203@qq.com>
2019-07-03 16:53:56 +0800

    add

--- ecommerce/src/PlantDatas/index.js
+++ ecommerce/src/PlantDatas/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import ReactDOM from "react-dom";
 import Nav from '../Component/Nav' 
+import LeftNavPane from '../Component/LeftNavPane'
 import './index.less'
 class PlantDatas extends Component {
     render () {
         return(
             <div className = "">
                 <Nav></Nav>
+                <LeftNavPane></LeftNavPane>
             </div>
         )
     }
