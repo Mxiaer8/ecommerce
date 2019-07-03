a84a74187c1ea790a14419fd9a0118a0f4a42850
Mxiaer8 <905594203@qq.com>
2019-07-03 10:20:08 +0800

    add nav

--- ecommerce/src/Component/Nav/index.jsx
+++ ecommerce/src/Component/Nav/index.jsx
@@ -30,48 +30,50 @@ class Nav extends Component {
         return (
             <div className="nav">
                 <div className="nav_top">
-                    <span>您好， 山顶村荔枝生成物联网平台欢迎您！</span>
+                    <span>您好， 山顶村荔枝生产物联网平台欢迎您！</span>
                     <div className="login_register">
                         <span><IconFont type="iconjinru" />登录</span>
                         <span><IconFont type="iconzhuce" />注册</span>
                     </div>
                 </div>
                 <div className="menu">
-                    <img></img>
+                    <div className="logo">
+                        <img src={require('../../static/images/logo.png')}></img>
+                    </div>
                     <div className="menu_item">
                         <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
                             <Menu.Item key="home">
-                                <a href = '/Home'>首页</a>
+                                <a href='/Home'>首页</a>
                             </Menu.Item>
                             <SubMenu
                                 title={
                                     <span className="submenu-title-wrapper plantDatas">种植数据</span>
                                 }
                             >
-                                <Menu.Item key="setting:1" className = "item">环境数据</Menu.Item>
-                                <Menu.Item key="setting:2" className = "item">果园图像</Menu.Item>
-                                <Menu.Item key="setting:3" className = "item">智慧栽培</Menu.Item>
-                                <Menu.Item key="setting:5" className = "item">智能识别</Menu.Item>
-                                <Menu.Item key="setting:6" className = "item">病虫害防治</Menu.Item>
-                                <Menu.Item key="setting:7" className = "item">更多功能</Menu.Item>
+                                <Menu.Item key="setting:1" className="item">环境数据</Menu.Item>
+                                <Menu.Item key="setting:2" className="item">果园图像</Menu.Item>
+                                <Menu.Item key="setting:3" className="item">智慧栽培</Menu.Item>
+                                <Menu.Item key="setting:5" className="item">智能识别</Menu.Item>
+                                <Menu.Item key="setting:6" className="item">病虫害防治</Menu.Item>
+                                <Menu.Item key="setting:7" className="item">更多功能</Menu.Item>
                             </SubMenu>
                             <SubMenu
                                 title={
                                     <span className="submenu-title-wrapper plantDatas">荔枝产品</span>
                                 }
                             >
-                                <Menu.Item key="setting:8" className = "item">产品信息</Menu.Item>
-                                <Menu.Item key="setting:9" className = "item">加工技术</Menu.Item>
-                                <Menu.Item key="setting:10" className = "item">更多</Menu.Item>
+                                <Menu.Item key="setting:8" className="item">产品信息</Menu.Item>
+                                <Menu.Item key="setting:9" className="item">加工技术</Menu.Item>
+                                <Menu.Item key="setting:10" className="item">更多</Menu.Item>
                             </SubMenu>
                             <SubMenu
                                 title={
                                     <span className="submenu-title-wrapper plantDatas">荔枝文化</span>
                                 }
                             >
-                                <Menu.Item key="setting:11" className = "item">荔枝简介</Menu.Item>
-                                <Menu.Item key="setting:12" className = "item">价值分析</Menu.Item>
-                                <Menu.Item key="setting:13" className = "item">更多</Menu.Item>
+                                <Menu.Item key="setting:11" className="item">荔枝简介</Menu.Item>
+                                <Menu.Item key="setting:12" className="item">价值分析</Menu.Item>
+                                <Menu.Item key="setting:13" className="item">更多</Menu.Item>
                             </SubMenu>
                             <Menu.Item key="contract">联系我们</Menu.Item>
                         </Menu>
