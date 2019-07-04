5251ebe196b942d9abe839cef3e8ba4b6504ac93
Mxiaer8 <905594203@qq.com>
2019-07-04 16:45:20 +0800

    add leftMenu

--- ecommerce/src/Config/IconFont.js
+++ ecommerce/src/Config/IconFont.js
@@ -1,7 +1,7 @@
 import { Icon } from 'antd';
  
 const IconFont = Icon.createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/font_1269215_pvpe7klr6i.js'
+  scriptUrl: '//at.alicdn.com/t/font_1269215_ie8493qfg69.js'
 });
  
 export default IconFont;
