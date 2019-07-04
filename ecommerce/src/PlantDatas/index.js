0a15c388ce5eea637c7a04f550ddf1313c3f2626
Mxiaer8 <905594203@qq.com>
2019-07-04 23:42:24 +0800

    add content

--- ecommerce/src/PlantDatas/index.js
+++ ecommerce/src/PlantDatas/index.js
@@ -4,12 +4,30 @@ import Nav from '../Component/Nav'
 import LeftNavPane from '../Component/LeftNavPane'
 import './index.less'
 class PlantDatas extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            nav_title: '种植数据'
+        }
+    }
+
+
+
     render() {
         return (
             <div className="plantDatas">
                 <Nav></Nav>
                 <div className="content">
                     <LeftNavPane></LeftNavPane>
+                    <div className = "main_content">
+                        <div className = "nav">
+                            <span className = "title">数据分析</span>
+                            <div className = "position">
+                                <span>您的位置:</span>
+                            </div>
+                        </div>
+                        <div></div>
+                    </div>
                 </div>
             </div>
         )
