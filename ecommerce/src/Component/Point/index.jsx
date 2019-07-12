618976bdabff2205c38986c094cac776a32548e7
Mxiaer8 <905594203@qq.com>
2019-07-12 16:34:37 +0800

    add

---
+++ ecommerce/src/Component/Point/index.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+
+class Point extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            title: this.props.title,
+            imageUrl : this.props.imageUrl
+        }
+    }
+
+    render() {
+        return(
+            <div className = 'point'>
+                <span className = 'title'>{this.state.title}</span>
+                <img src = {this.state.imageUrl}></img>
+            </div>
+        )
+    }
+}
+
+export default Point
